25fc5b34b7a6fb3f378675faa96f16523ec760bf
Yuvraj Patadia <ypata001@ucr.edu>
2024-08-10 14:22:21 -0700

    fixed header active refresh issue

--- my-app/src/components/Header/AppHeader.js
+++ my-app/src/components/Header/AppHeader.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout, Menu, Button } from "antd";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { HomeOutlined, HistoryOutlined, StarOutlined, LogoutOutlined } from "@ant-design/icons";
 import { useAuth } from "../Context/AuthContext";
 
@@ -9,6 +9,7 @@ const { Header } = Layout;
 const AppHeader = () => {
   const { logout } = useAuth();
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogout = () => {
     logout();
@@ -19,14 +20,14 @@ const AppHeader = () => {
     <Header className="header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["home"]} style={{ flexGrow: 1 }}>
-          <Menu.Item key="home" icon={<HomeOutlined />}>
+        <Menu theme="dark" mode="horizontal" selectedKeys={[location.pathname]} style={{ flexGrow: 1 }}>
+          <Menu.Item key="/home" icon={<HomeOutlined />}>
             <Link to="/home">Home</Link>
           </Menu.Item>
-          <Menu.Item key="history" icon={<HistoryOutlined />}>
+          <Menu.Item key="/history" icon={<HistoryOutlined />}>
             <Link to="/history">History</Link>
           </Menu.Item>
-          <Menu.Item key="favourites" icon={<StarOutlined />}>
+          <Menu.Item key="/favourites" icon={<StarOutlined />}>
             <Link to="/favourites">Favourites</Link>
           </Menu.Item>
         </Menu>
